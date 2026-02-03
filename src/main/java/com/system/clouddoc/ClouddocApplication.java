c7c2dbc3c7c540ec891b1ae4534713f2f95aae36
Carlos Torres Perez <carlos30nov14@gmail.com>
2026-02-02 23:39:47 -0600

    Alta proyecto

---
+++ src/main/java/com/system/clouddoc/ClouddocApplication.java
@@ -0,0 +1,13 @@
+package com.system.clouddoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouddocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClouddocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/clouddoc/ClouddocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.clouddoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClouddocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
